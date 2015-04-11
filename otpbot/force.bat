669ae2c864b11c18cab53787aca6fb935d991677
crashdemons <crashenator+gh@gmail.com>
2015-04-11 00:36:10 -0500

    ***Manual Release process started - commiting any unsubmitted changes first

--- otpbot/force.bat
+++ otpbot/force.bat
@@ -4,7 +4,11 @@ call commit "***Manual Release process started - commiting any unsubmitted chang
 echo "returned"
 
 REM del C:\Users\Crash\Desktop\otp22\code\IRC\otpbot\WC\Release.ver
+GetNextVersion C:\Users\Crash\Desktop\otp22\code\IRC\otpbot\WC\Release.ver
+
 GetNextVersion C:\Users\Crash\Desktop\otp22\code\IRC\otpbot\WC\Release.ver > C:\Users\Crash\Desktop\otp22\code\IRC\otpbot\WC\Release.ver
-commit "***Manual release performed - no archive generated"
+
+type C:\Users\Crash\Desktop\otp22\code\IRC\otpbot\WC\Release.ver
+REM commit "***Manual release performed - no archive generated"
 exit /B
 
