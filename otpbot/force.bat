5572756d702b022f3643555fa2095c8d27b3d9d6
crashdemons <crashenator+gh@gmail.com>
2017-02-10 18:34:05 -0600

    version change

--- otpbot/force.bat
+++ otpbot/force.bat
@@ -3,7 +3,7 @@
 call commit "***Manual Release process started - commiting any unsubmitted changes first"
 echo "returned"
 
-GetNextVersion C:\Users\Crash\Desktop\otp22\code\IRC\otpbot\WC\Release.ver
+GetNextVersion .\WC\Release.ver
 
 commit "***Manual release performed"
 exit /B
